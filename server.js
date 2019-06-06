6d29d6710bc81ec84a5d796fb9dd033d92add114
Anisha Dhana <anishadhana@gmail.com>
2019-06-07 10:18:12 +1200

    server setup

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 
-const server
+const server = express()
+
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+server.use('/puppies', puppiesRoutes)
+server.use('/', homeRoutes)
+
+module.exports = server
